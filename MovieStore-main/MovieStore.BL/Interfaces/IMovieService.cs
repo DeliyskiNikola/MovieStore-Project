78d7f12e0f8156aa4721128c451901cf0d9cc278
DeliyskiNikola <koliostudenta@abv.bg>
2024-12-09 19:17:28 +0200

    commit

---
+++ MovieStore-main/MovieStore.BL/Interfaces/IMovieService.cs
@@ -0,0 +1,15 @@
+﻿using MovieStore.Models.DTO;
+
+namespace MovieStore.BL.Interfaces
+{
+    public interface IMovieService
+    {
+        List<Movie> GetAllMovies();
+
+        void AddMovie(Movie movie);
+        Movie? GetMovieById(string id);
+        void DeleteMovie(string id);
+        void UpdateMovie(Movie movie);
+        Actor? GetActorById(int id);
+    }
+}
---
+++ MovieStore-main/MovieStore.DL/Interfaces/IMovieRepository.cs
@@ -0,0 +1,14 @@
+﻿using MovieStore.Models.DTO;
+
+namespace MovieStore.DL.Interfaces
+{
+    public interface IMovieRepository
+    {
+        List<Movie> GetAllMovies();
+        void AddMovie(Movie movie);
+
+        Movie? GetMovieById(string id);
+        void DeleteMovie(string id);
+        void UpdateMovie(Movie movie);
+    }
+}
