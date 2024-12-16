a0f9fd4b99c8590383fda493940f02f422a25755
DeliyskiNikola <koliostudenta@abv.bg>
2024-12-16 19:03:48 +0200

    commit

--- MovieStore-main/MovieStore.Models/DTO/Actor.cs
+++ MovieStore-main/MovieStore.Models/DTO/Actor.cs
@@ -2,6 +2,7 @@ namespace MovieStore.Models.DTO;
 
 public class Actor
 {
+    public static string Id { get; set; }
     public int ID { get; set; }
     
     public string Name { get; set; } = string.Empty;
